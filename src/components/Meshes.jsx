75b38752dda4293fc735a0319f2ac67d07ee0725
cho-hyeonjin <gusls2005@gmail.com>
2024-04-22 22:55:16 +0900

    Create Meshes with Materials

--- src/components/Meshes.jsx
+++ src/components/Meshes.jsx
@@ -6,6 +6,7 @@ import {
   Plane,
   Sphere,
   Torus,
+  TorusKnot,
 } from "@react-three/drei";
 import * as THREE from "three";
 
@@ -20,23 +21,74 @@ export const Meshes = () => {
       <Plane args={[40, 40]} rotation-x={-Math.PI / 2} receiveShadow>
         <meshStandardMaterial />
       </Plane>
+
       <Box args={[1, 1, 1]} position-y={0.5} castShadow>
         <meshStandardMaterial color={0xff0000} />
       </Box>
+
       <Sphere args={[1]} position={[0, 1, 3]} material-color={0xffff00} />
+
       <Circle
         args={[1]}
         position={[0, 1, -3]}
         material-color={"violet"}
         material-side={THREE.DoubleSide}
       />
+
       <Cone args={[1, 2]} position={[3, 1, 3]} material-color={"turquoise"} />
+
       <Cylinder
         args={[2, 1, 2]}
         position={[3, 1, -3]}
         material-color={"skyblue"}
       />
+
       <Torus args={[1, 0.2]} position={[-3, 1.2, -3]} material-color={"pink"} />
+
+      <TorusKnot
+        args={[1, 0.2, 128, 128, 2, 3]}
+        position={[-3, 1.6, 0]}
+        castShadow
+        receiveShadow
+      >
+        <meshStandardMaterial color={0x0000ff} roughness={0.5} metalness={1} />
+      </TorusKnot>
+
+      <TorusKnot
+        args={[1, 0.2, 128, 128, 2, 3]}
+        position={[-7, 1.6, 0]}
+        castShadow
+        receiveShadow
+      >
+        <meshLambertMaterial
+          color={0x0aaff0}
+          emissive={0xff0000}
+          emissiveIntensity={0.5}
+        />
+      </TorusKnot>
+
+      <TorusKnot
+        args={[1, 0.2, 128, 128, 2, 3]}
+        position={[-11, 1.6, 0]}
+        castShadow
+        receiveShadow
+      >
+        <meshPhongMaterial
+          color={0xff0000}
+          emissive={0x00ff00}
+          emissiveIntensity={0.5}
+          specular={0x0000ff}
+        />
+      </TorusKnot>
+
+      <TorusKnot
+        args={[1, 0.2, 128, 128, 2, 3]}
+        position={[-15, 1.6, 0]}
+        castShadow
+        receiveShadow
+      >
+        <meshDepthMaterial opacity={0.5} />
+      </TorusKnot>
     </>
   );
 };
