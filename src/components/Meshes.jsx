b03068073653fc613a10bf217512db3aa6b6578d
cho-hyeonjin <gusls2005@gmail.com>
2024-04-22 21:10:07 +0900

    ♻️Refactor: Replace the '<mesh>' Element in the 'Meshes' Component with the 'Box' Component from the 'drei' Library

--- src/components/Meshes.jsx
+++ src/components/Meshes.jsx
@@ -1,9 +1,14 @@
+import { Box } from "@react-three/drei";
+
 /* eslint-disable react/no-unknown-property */
 export const Meshes = () => {
   return (
-    <mesh position={[0, 0, 0]}>
+    <>
+      {/* <mesh position={[0, 0, 0]}>
       <boxGeometry args={[1, 1, 1]} />
       <meshBasicMaterial color={0xff0000} />
-    </mesh>
+    </mesh> */}
+      <Box args={[1, 1, 1]} material-color={0xff0000} />
+    </>
   );
 };
